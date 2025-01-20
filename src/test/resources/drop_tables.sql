a673b5b4fa7450d686d087fc26ffc4ef0b1742aa
cheroliv <cheroliv@laposte.net>
2025-01-20 23:33:09 +0100

    feat: Add SQL script to drop tables for testing
    This commit introduces a new SQL script, `drop_tables.sql`, designed to drop all relevant tables in a database.
    This script includes commands to drop multiple tables: `jhi_user_authority`, `user_activation`, `databasechangelog`, `databasechangeloglock`, `user_authority`, `"user"`, `"authority"`, `"jhi_user"`, and `"jhi_authority"`. This is useful for cleaning up the database during testing, ensuring a fresh state before running tests, and avoiding conflicts.

---
+++ src/test/resources/drop_tables.sql
@@ -0,0 +1,11 @@
+drop TABLE if EXISTS jhi_user_authority;
+drop TABLE if EXISTS public.user_activation;
+drop TABLE if EXISTS public.databasechangelog;
+drop TABLE if EXISTS databasechangeloglock;
+drop TABLE if EXISTS user_authority;
+drop TABLE if EXISTS "user";
+drop TABLE if EXISTS "authority";
+drop TABLE if EXISTS "jhi_user";
+drop TABLE if EXISTS "jhi_authority";
+
+
